7a8dd4d770d63f14b4e0b144919a60117fcc9887
Pradnya <pradnya24.dalvi@gmail.com>
2019-02-14 14:48:00 +0530

    create file metadata

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var upload = multer({ dest: './public' });
+// require and use "multer"...
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.route('/api/fileanalyse')
+.post(upload.single('upfile'),(req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
